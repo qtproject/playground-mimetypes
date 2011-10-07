95975599f7237a5ddbcb7de0c0aa19d0ac3a6ece
Wolf-Michael Bolle <wolf-michael.bolle@nokia.com>
2011-10-07 16:25:53 +0200

    Updated the documentation.

--- src/qmime_global.h
+++ src/qmime_global.h
@@ -1,3 +1,23 @@
+/**************************************************************************
+**
+** This file is part of QMime
+**
+** Based on Qt Creator source code
+**
+** Qt Creator Copyright (c) 2011 Nokia Corporation and/or its subsidiary(-ies).
+**
+**
+** GNU Lesser General Public License Usage
+**
+** This file may be used under the terms of the GNU Lesser General Public
+** License version 2.1 as published by the Free Software Foundation and
+** appearing in the file LICENSE.LGPL included in the packaging of this file.
+** Please review the following information to ensure the GNU Lesser General
+** Public License version 2.1 requirements will be met:
+** http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+**
+**************************************************************************/
+
 #ifndef QMIME_GLOBAL_H
 #define QMIME_GLOBAL_H
 
