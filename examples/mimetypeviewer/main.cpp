71c02be6db892703235f55c43354767114ec9891
Wolf-Michael Bolle <wolf-michael.bolle@nokia.com>
2011-10-17 15:37:02 +0200

    Compatibility for Qt 4 and 5.

--- examples/mimetypeviewer/main.cpp
+++ examples/mimetypeviewer/main.cpp
@@ -1,5 +1,10 @@
-#include <QtGui/QApplication>
 #include "mimetypeviewer.h"
+
+#if (QT_VERSION >= QT_VERSION_CHECK(5, 0, 0))
+#include <QtWidgets/QApplication>
+#else
+#include <QtGui/QApplication>
+#endif
 
 int main(int argc, char *argv[])
 {
