7977ce3e789e1006246a247ab052dce7d6eb0fb0
Wolf-Michael Bolle <wolf-michael.bolle@nokia.com>
2011-10-17 14:43:13 +0200

    Renamed a variable to its actual meaning.

--- tests/auto/qmimedatabase/tst_qmimedatabase.cpp
+++ tests/auto/qmimedatabase/tst_qmimedatabase.cpp
@@ -44,7 +44,7 @@ void tst_qmimedatabase::initTestCase()
 void tst_qmimedatabase::findByName_data()
 {
     QTest::addColumn<QString>("filePath");
-    QTest::addColumn<QString>("mimeType");
+    QTest::addColumn<QString>("mimeTypeName");
     QTest::addColumn<QString>("xFail");
 
     QString prefix = QLatin1String(SRCDIR "testfiles/");
@@ -65,19 +65,19 @@ void tst_qmimedatabase::findByName_data()
         QVERIFY(list.size() >= 2);
 
         QString filePath = list.at(0);
-        QString mimeType = list.at(1);
+        QString mimeTypeType = list.at(1);
         QString xFail;
         if (list.size() == 3)
             xFail = list.at(2);
 
-        QTest::newRow(filePath.toLatin1().constData()) << prefix + filePath << mimeType << xFail;
+        QTest::newRow(filePath.toLatin1().constData()) << prefix + filePath << mimeTypeType << xFail;
     }
 }
 
 void tst_qmimedatabase::findByName()
 {
     QFETCH(QString, filePath);
-    QFETCH(QString, mimeType);
+    QFETCH(QString, mimeTypeName);
     QFETCH(QString, xFail);
 
     //qDebug() << Q_FUNC_INFO << filePath;
@@ -97,9 +97,9 @@ void tst_qmimedatabase::findByName()
     const bool shouldFail = (xFail.length() >= 1 && xFail.at(0) == QLatin1Char('x'));
     if (shouldFail) {
         // Expected to fail
-        QVERIFY2(resultMimeTypeName != mimeType, qPrintable(resultMimeTypeName));
+        QVERIFY2(resultMimeTypeName != mimeTypeName, qPrintable(resultMimeTypeName));
     } else {
-        QCOMPARE(resultMimeTypeName, mimeType);
+        QCOMPARE(resultMimeTypeName, mimeTypeName);
     }
 }
 
@@ -111,7 +111,7 @@ void tst_qmimedatabase::findByData_data()
 void tst_qmimedatabase::findByData()
 {
     QFETCH(QString, filePath);
-    QFETCH(QString, mimeType);
+    QFETCH(QString, mimeTypeName);
     QFETCH(QString, xFail);
 
     QFile f(filePath);
@@ -121,10 +121,10 @@ void tst_qmimedatabase::findByData()
     const QString resultMimeTypeName = database.findByData(data).name();
     if (xFail.length() >= 2 && xFail.at(1) == QLatin1Char('x')) {
         // Expected to fail
-        QVERIFY2(resultMimeTypeName != mimeType, qPrintable(resultMimeTypeName));
+        QVERIFY2(resultMimeTypeName != mimeTypeName, qPrintable(resultMimeTypeName));
     }
     else {
-        QCOMPARE(resultMimeTypeName, mimeType);
+        QCOMPARE(resultMimeTypeName, mimeTypeName);
     }
 }
 
@@ -136,17 +136,17 @@ void tst_qmimedatabase::findByFile_data()
 void tst_qmimedatabase::findByFile()
 {
     QFETCH(QString, filePath);
-    QFETCH(QString, mimeType);
+    QFETCH(QString, mimeTypeName);
     QFETCH(QString, xFail);
 
     const QString resultMimeTypeName = database.findByFile(QFileInfo(filePath)).name();
     //qDebug() << Q_FUNC_INFO << filePath << "->" << resultMimeTypeName;
     if (xFail.length() >= 3 && xFail.at(2) == QLatin1Char('x')) {
         // Expected to fail
-        QVERIFY2(resultMimeTypeName != mimeType, qPrintable(resultMimeTypeName));
+        QVERIFY2(resultMimeTypeName != mimeTypeName, qPrintable(resultMimeTypeName));
     }
     else {
-        QCOMPARE(resultMimeTypeName, mimeType);
+        QCOMPARE(resultMimeTypeName, mimeTypeName);
     }
 }
 
