8b14f92bad5148987578540dd2b0c4cea44a0af1
David Faure <faure@kde.org>
2011-10-29 10:15:31 +0200

    Comment out test, was a shared-mime-info bug, fixed it there.

--- tests/auto/qmimedatabase/tst_qmimedatabase.cpp
+++ tests/auto/qmimedatabase/tst_qmimedatabase.cpp
@@ -74,7 +74,9 @@ void tst_qmimedatabase::test_findByName_data()
 
     QTest::newRow("text") << "textfile.txt" << "text/plain";
     QTest::newRow("case-insensitive search") << "textfile.TxT" << "text/plain";
-    QTest::newRow("case-insensitive match on a non-lowercase glob") << "foo.z" << "application/x-compress";
+
+    // Needs shared-mime-info > 0.91. Earlier versions wrote .Z to the mime.cache file...
+    //QTest::newRow("case-insensitive match on a non-lowercase glob") << "foo.z" << "application/x-compress";
 
     QTest::newRow("case-sensitive uppercase match") << "textfile.C" << "text/x-c++src";
     QTest::newRow("case-sensitive lowercase match") << "textfile.c" << "text/x-csrc";
