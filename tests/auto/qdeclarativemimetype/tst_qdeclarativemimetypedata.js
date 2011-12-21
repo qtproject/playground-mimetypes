4c340c7f8bc37e48e072c4a149ca760343c646d6
Wolf-Michael Bolle <wolf-michael.bolle@nokia.com>
2011-12-21 13:58:36 +0100

    Ran JavaScript code through JSLint and JSLint.

--- tests/auto/qdeclarativemimetype/tst_qdeclarativemimetypedata.js
+++ tests/auto/qdeclarativemimetype/tst_qdeclarativemimetypedata.js
@@ -1,19 +1,21 @@
 function equalStrings(lhs, rhs) {
-    return lhs == rhs
+    return lhs == rhs;
 }
 
 function equalArrays(lhs, rhs) {
+    var ix;
+
     if (lhs.length != rhs.length) {
         return false;
     }
 
-    for (var ix = 0; ix < lhs.length; ++ix) {
+    for (ix = 0; ix < lhs.length; ix = ix + 1) {
         if (typeof lhs[ix] != typeof rhs[ix]) {
             return false;
         }
-        
+
         if (lhs[ix] != rhs[ix]) {
-            return false
+            return false;
         }
     }
 
@@ -21,19 +23,19 @@ function equalArrays(lhs, rhs) {
 }
 
 function mimeTypeName() {
-    return "image/png"
+    return "image/png";
 }
 
 function equalMimeTypeName(lhs, rhs) {
-    return equalStrings(lhs, rhs)
+    return equalStrings(lhs, rhs);
 }
 
 //function mimeTypeAliases() {
-//    return [ "picture/png" ]
+//    return [ "picture/png" ];
 //}
 
 //function equalMimeTypeAliases(lhs, rhs) {
-//    return equalArrays(lhs, rhs)
+//    return equalArrays(lhs, rhs);
 //}
 
 //function mimeTypeComment() {
@@ -41,37 +43,37 @@ function equalMimeTypeName(lhs, rhs) {
 //}
 
 //function equalMimeTypeComment(lhs, rhs) {
-//    return equalStrings(lhs, rhs)
+//    return equalStrings(lhs, rhs);
 //}
 
 function mimeTypeGenericIconName() {
-    return "/usr/share/icons/oxygen/64x64/mimetypes/image-x-generic.png"
+    return "/usr/share/icons/oxygen/64x64/mimetypes/image-x-generic.png";
 }
 
 function equalMimeTypeGenericIconName(lhs, rhs) {
-    return equalStrings(lhs, rhs)
+    return equalStrings(lhs, rhs);
 }
 
 function mimeTypeIconName() {
-    return "/usr/share/icons/oxygen/64x64/mimetypes/image-x-generic.png"
+    return "/usr/share/icons/oxygen/64x64/mimetypes/image-x-generic.png";
 }
 
 function equalMimeTypeIconName(lhs, rhs) {
-    return equalStrings(lhs, rhs)
+    return equalStrings(lhs, rhs);
 }
 
 function mimeTypeGlobPatterns() {
-    return [ "*.png" ]
+    return [ "*.png" ];
 }
 
 function equalMimeTypeGlobPatterns(lhs, rhs) {
-    return equalArrays(lhs, rhs)
+    return equalArrays(lhs, rhs);
 }
 
 function mimeTypeSuffixes() {
-    return [ ".png" ]
+    return [ ".png" ];
 }
 
 function equalMimeTypeSuffixes(lhs, rhs) {
-    return equalArrays(lhs, rhs)
+    return equalArrays(lhs, rhs);
 }
