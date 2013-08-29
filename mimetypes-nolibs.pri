qtproject/playground-mimetypes

commit 10517652f65f05120432ea05883977e39ed7b8be

Fixes development header and lib install directory
PREFIX variable added. Let packagers and users choose the install prefix.

Headers installed to $${INCLUDEDIR}/QtMimeTypes. Installing it to
$${INCLUDEDIR}/qt5/QtMimeTypes makes no sense. This is an Qt4 project.
Libraries installed to $${LIBDIR}.

maemo5 "special" include and lib path dropped. Just use the PREFIX
variable.

Use example:
	qmake PREFIX=/usr/local

Change-Id: I4fc173e7999ea7ffdd4dfe26f8a142a822fa7da9
Signed-off-by: Luís Pereira <luis.artur.pereira@gmail.com>
Reviewed-by: David Faure (KDE) <faure@kde.org>

mimetypes-nolibs.pri
@@ -1,3 +1,6 @@
+!isEmpty(MIMETYPES_PRI_INCLUDED):error("mimetypes-no-libs.pri already included")
+MIMETYPES_PRI_INCLUDED = 1
+
 INCLUDEPATH += $$PWD/include/QtMimeTypes
 INCLUDEPATH += $$PWD/src/mimetypes/inqt5
 INCLUDEPATH += $$PWD/src/mimetypes $$PWD/src/imports/mimetypes
@@ -8,4 +11,13 @@ mac|darwin: {
     QMAKE_CXXFLAGS += -ansi -Wc++0x-compat
 } else {
     QMAKE_CXXFLAGS += -std=c++0x
+}
+
+unix {
+    isEmpty(PREFIX) {
+        PREFIX = /usr
+    }
+
+    INCLUDEDIR = $${PREFIX}/include
+    LIBDIR = ${{PREFIX}/lib
 }

include/QtMimeTypes/QtMimeTypes.pro
@@ -1,3 +1,5 @@
+include(../../mimetypes-nolibs.pri)
+
 contains($$list($$[QT_VERSION]),4.[6-9].*) {
     TEMPLATE = subdirs
 } else {
@@ -8,10 +10,6 @@ the_includes.files += QMimeDatabase \
                       QMimeType \
 
 unix:!symbian {
-    maemo5 {
-        the_includes.path = /opt/usr/include/qt5/QtMimeTypes
-    } else {
-        the_includes.path = /usr/include/qt5/QtMimeTypes
-    }
+    the_includes.path = $${INCLUDEDIR}/QtMimeTypes
     INSTALLS += the_includes
 }

qtmimetypes.pro
@@ -1,3 +1,4 @@
+include(mimetypes-nolibs.pri)
 include(doc/doc.pri)
 
 # check qt version

src/mimetypes/mimetypes.pro
@@ -61,12 +61,7 @@ symbian {
 }
 
 unix:!symbian {
-    maemo5 {
-        target.path = /opt/usr/lib
-        the_includes.path = /opt/usr/include/qt5/QtMimeTypes
-    } else {
-        target.path = /usr/lib
-        the_includes.path = /usr/include/qt5/QtMimeTypes
-    }
+    target.path = ${{LIBDIR}
+    the_includes.path = $${INCLUDEDIR}/QtMimeTypes
     INSTALLS += target the_includes
 }

commit 7f34b73db42b13cc7e001a4564e6545d547146f1

Fixes a bug in the LIBDIR install path
Introduced by commit 10517652f65f05120432ea05883977e39ed7b8be.
It made libQtMimeTypes installation fail.

Change-Id: I7c512b5d29bcfa06896a2df71f98b9c58db06817
Signed-off-by: Luís Pereira <luis.artur.pereira@gmail.com>
Reviewed-by: David Faure (KDE) <faure@kde.org>

mimetypes-nolibs.pri
@@ -19,5 +19,5 @@ unix {
     }
 
     INCLUDEDIR = $${PREFIX}/include
-    LIBDIR = ${{PREFIX}/lib
+    LIBDIR = $${PREFIX}/lib
 }